38f9c189064e0b3026d90455ec321220823ff1c1
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-01-27 22:07:20 +0530

    Day 01: Implemented Binary Search in Java

---
+++ Day_01/Binary_Serach.java
@@ -0,0 +1,25 @@
+package Day_01;
+
+public class Binary_Serach {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = binarySearch(arr, target);
+        System.out.println("Index of " + target + " is: " + result);
+    }
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
