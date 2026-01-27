46364e88381f143a50fb7da160e76beb6ef7f900
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-01-27 22:57:13 +0530

    Solution for LeetCode 34: Find First and Last Position

---
+++ Day_01/Leetcode34.java
@@ -0,0 +1,33 @@
+package Day_01;
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int [] result = {-1,-1};
+        result[0]=binarySearch(nums,target,true);
+        result[1]=binarySearch(nums,target,false);
+        return result;
+    }
+    public static int binarySearch(int[] nums,int target,boolean isFirst){       int ans=-1;
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid= left +(right - left)/2;
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+            else if(nums[mid]>target){
+            right=mid-1;
+            }
+            else{
+                ans=mid;
+                if(isFirst){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+
+        }
+        return ans;
+    }
+}
