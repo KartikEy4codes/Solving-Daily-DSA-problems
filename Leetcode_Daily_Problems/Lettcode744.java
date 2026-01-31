cbf50d042131687ff5be8cdf102e5ad0c27d1e2b
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-01-31 16:51:56 +0530

    Solve daily leetcode

---
+++ Leetcode_Daily_Problems/Lettcode744.java
@@ -0,0 +1,29 @@
+package Leetcode_Daily_Problems;
+
+public class Lettcode744 {
+    class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char result= binSearchceil(letters,target);
+        return result;
+        
+    }
+    public static char binSearchceil(char[] arr,char target){
+        char ans='$';
+        int st=0;
+        int end=arr.length-1;
+        while(st<=end){
+            int mid=st + (end-st)/2;
+            if(arr[mid]<=target){
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+                ans=arr[mid];
+            }
+
+        }
+        return (ans=='$')?arr[0]:ans;
+    }
+}
+    
+}
