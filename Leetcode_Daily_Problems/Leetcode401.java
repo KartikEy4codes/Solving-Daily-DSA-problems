1cb267bc86ea6e21f5eebfe7e3b5cba43750ef07
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-02-17 20:00:15 +0530

    feat: add solution for reading binary watch

---
+++ Leetcode_Daily_Problems/Leetcode401.java
@@ -0,0 +1,19 @@
+public class Leetcode401 {
+    class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) { 
+                int totalBits = Integer.bitCount(h) + Integer.bitCount(m); 
+                if (totalBits == turnedOn) {
+                    String time = h + ":" + 
+                                  (m < 10 ? "0" + m : m);
+                    result.add(time);
+                }
+            }
+        }
+        return result;
+    }
+}
+    
+}
