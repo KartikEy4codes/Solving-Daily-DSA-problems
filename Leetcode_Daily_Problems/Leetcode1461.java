992246a1fd68d5908dabf49e29adc367cf302158
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-02-23 17:23:22 +0530

    Check If a String Contains All Binary Codes of Size K

---
+++ Leetcode_Daily_Problems/Leetcode1461.java
@@ -0,0 +1,16 @@
+public class Leetcode1461 {
+    import java.util.*;
+
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        HashSet<String> st = new HashSet<>();
+
+        for (int i = 0; i + k <= s.length(); i++) {
+            st.add(s.substring(i, i + k));
+        }
+
+        return st.size() == (int)Math.pow(2, k);
+    }
+}
+    
+}
