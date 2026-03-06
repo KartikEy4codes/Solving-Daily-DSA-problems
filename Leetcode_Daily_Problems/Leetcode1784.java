86d682eac70fcae7b8a8c1a59d4b57d306bf1fce
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-03-06 15:27:33 +0530

    lt

---
+++ Leetcode_Daily_Problems/Leetcode1784.java
@@ -0,0 +1,15 @@
+package Leetcode_Daily_Problems;
+
+public class Leetcode1784 {
+
+    class Solution {
+    public boolean checkOnesSegment(String s) {
+      char[] c=s.toCharArray();
+      for(int i=1;i<c.length;i++){
+        if(c[i]=='1' && c[i-1]=='0') return false;
+      }
+      return true;
+        
+    }
+}
+}
