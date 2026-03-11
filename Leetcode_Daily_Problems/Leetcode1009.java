576aec9afed519e997918a80983a23871f0d0e60
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-03-11 22:08:50 +0530

    lt

---
+++ Leetcode_Daily_Problems/Leetcode1009.java
@@ -0,0 +1,14 @@
+package Leetcode_Daily_Problems;
+
+public class Leetcode1009 {
+
+    class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        int mask = n;
+        for (int i = 0; i <= 4; i++)
+            mask |= mask >> (1 << i);
+        return ~n & mask;
+    }
+}
+}
