f51215c7f3265b6f4d6064bb9b41531282957e85
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-03-13 20:45:49 +0530

    lt

--- Leetcode_Daily_Problems/Leetcode3296.java
+++ Leetcode_Daily_Problems/Leetcode3296.java
@@ -1,3 +1,21 @@
 public class Leetcode3296 {
+    class Solution {
+    public long minNumberOfSeconds(int height, int[] times) {
+        long low = 1, high = 10000000000000000L;
+
+        while (low < high) {
+            long mid = (low + high) >> 1;
+            long total = 0;
+            for (int i = 0; i < times.length && total < height; i++)
+                total += (long) (Math.sqrt((double) mid / times[i] * 2 + 0.25) - 0.5);
+            if (total >= height)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+}
     
 }
