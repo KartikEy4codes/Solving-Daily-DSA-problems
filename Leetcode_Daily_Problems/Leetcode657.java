c4febbd973eeba747c9d888fb2c1d396078d7765
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-04-05 11:33:54 +0530

    lt

---
+++ Leetcode_Daily_Problems/Leetcode657.java
@@ -0,0 +1,15 @@
+package Leetcode_Daily_Problems;
+
+public class Leetcode657 {
+
+    class Solution {
+    public boolean judgeCircle(String moves) {
+        int[] ch = new int[26];
+        for (char move : moves.toCharArray()){
+            ch[move - 'A']++;
+        }
+        return ch['U' - 'A'] == ch['D' - 'A'] && 
+                ch['L' - 'A'] == ch['R' - 'A'];
+    }
+}
+}
