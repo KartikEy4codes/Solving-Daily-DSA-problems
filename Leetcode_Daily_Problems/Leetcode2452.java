d4e7a0102730722e0012534675f837c55e30ffaf
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-04-22 21:36:18 +0530

    lt

---
+++ Leetcode_Daily_Problems/Leetcode2452.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode2452 {
+    class Solution {
+
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> ans = new ArrayList<>();
+        for (String query : queries) {
+            for (String s : dictionary) {
+                int dis = 0;
+                for (int i = 0; i < query.length(); i++) {
+                    if (query.charAt(i) != s.charAt(i)) {
+                        dis++;
+                    }
+                }
+                if (dis <= 2) {
+                    ans.add(query);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+}
