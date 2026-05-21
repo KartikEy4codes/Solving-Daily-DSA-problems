1d71a402a9b442a6aefb4027e458429befcce29b
KartikEy4codes <kartiksrivastava12345@gmail.com>
2026-05-21 13:20:49 +0530

    lt

---
+++ Leetcode_Daily_Problems/Leetcode3043.java
@@ -0,0 +1,30 @@
+package Leetcode_Daily_Problems;
+
+public class Leetcode3043 {
+
+    class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        int commonPrefixLength = 0;
+        HashSet<Integer> prefixSet = new HashSet<>();
+        for (int num : arr1) {
+            while (num > 0) {
+                prefixSet.add(num);
+                num /= 10;
+            }
+        }
+        for (int num : arr2) {
+            while (num > 0) {
+                if (prefixSet.contains(num)) {
+                    int prefixLength = String.valueOf(num).length();
+                    if(prefixLength > commonPrefixLength) {
+                        commonPrefixLength = prefixLength;
+                    }
+                    break;
+                }
+                num /= 10;
+            }
+        }
+        return commonPrefixLength;
+    }
+}
+}
